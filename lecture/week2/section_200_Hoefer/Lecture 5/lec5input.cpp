a356d5b6b15094f2c7c5670f833be3680a9e8b00
CSCI 1300 <csci1300@colorado.edu>
2023-01-27 21:18:40 +0000

    Update CSCI 1300 Files

---
+++ lecture/week2/section_200_Hoefer/Lecture 5/lec5input.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // print prompt asking user for their favorite LETTER
+
+    // declare a destination point for the user input
+
+    // get the input from the user
+
+    // print out their favorite letter
+
+
+    // print out the INTEGER representation of their favorite letter 
+    // (remember our friend, the ASCII table?)
+    
+
+    return 0;
+}
---
+++ lecture/week2/section_200_Hoefer/Lecture 5/lec5multiInput.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // prompt user to enter two numbers at the same time (space separated)
+
+    // calculate the sum of the two numbers, print the sum
+
+    // calculate the absolute value of the difference of the two numbers, 
+
+
+    // print the absolute value of the difference
+
+
+    // calculate the value of the first int to the power of the second int
+    
+
+    return 0;
+}
